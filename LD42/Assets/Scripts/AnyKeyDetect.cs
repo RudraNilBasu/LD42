8be14bb36f077c4c65233a91e691d63b554ec688
Rudra Nil Basu <rudra.nil.basu.1996@gmail.com>
2018-08-14 02:39:54 +0530

    Audio: Background Music

--- LD42/Assets/Scripts/AnyKeyDetect.cs
+++ LD42/Assets/Scripts/AnyKeyDetect.cs
@@ -12,6 +12,9 @@ public class AnyKeyDetect : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		if (Input.GetKey(KeyCode.Escape)) {
+            Application.Quit();
+        }
 		if (Input.anyKey) {
             SceneManager.LoadScene("SampleScene");
         }
---
+++ LD42/Assets/Scripts/KeepInstance.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeepInstance : MonoBehaviour {
+	/*
+	   private static KeepInstance instance;
+
+	   public static KeepInstance Instance
+	   {
+	   get { return instance; }
+	   }
+
+	   private void Awake()
+	   {
+	// If no Player ever existed, we are it.
+	if (instance == null)
+	instance = this;
+	// If one already exist, it's because it came from another level.
+	else if (instance != this)
+	{
+	Destroy(gameObject);
+	return;
+	}
+	}
+	*/
+	private static bool created = false;
+
+	void Awake()
+	{
+		if (!created)
+		{
+			DontDestroyOnLoad(this.gameObject);
+			created = true;
+		}
+	}
+
+}
