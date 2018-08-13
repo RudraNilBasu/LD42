405587c28c3e6f1f2bad6f3a12f6f1f198bf3daf
Rudra Nil Basu <rudra.nil.basu.1996@gmail.com>
2018-08-14 00:55:37 +0530

    Main Menu

---
+++ LD42/Assets/Scripts/BlinkingEffect.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BlinkingEffect : MonoBehaviour {
+
+    Text m_text;
+
+    string c_text = "Press Any Key to Start";
+	// Use this for initialization
+	void Start () {
+        m_text = GetComponent<Text>();
+        if (m_text == null) {
+            Debug.LogError("No text found");
+        }
+        m_text.text = c_text;
+
+        StartCoroutine(Flip());
+	}
+	
+    IEnumerator Flip()
+    {
+        yield return new WaitForSeconds(0.5f);
+        m_text.text = "";
+        // gameObject.SetActive(false);
+
+        yield return new WaitForSeconds(0.4f);
+        m_text.text = c_text;
+        // gameObject.SetActive(true);
+        StartCoroutine(Flip());
+    }
+}
